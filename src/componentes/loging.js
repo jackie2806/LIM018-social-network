144f14400b08b8d11ba6621f6261c84e7ef6f77e
Yurema <yuremix.b.s@gmail.com>
2022-08-09 22:41:41 -0500

    ruteo

--- src/componentes/loging.js
+++ src/componentes/loging.js
@@ -3,11 +3,13 @@ export default () => {
     /* crear cuenta */
     const createAccount = `
     <img src='./img/Placematch__2_-removebg-preview 2.png' alt="logo"> <br>
-    <input id='name' placeholder='Nombre' /> <br>
-    <input id='email-loging' placeholder="ejemplo@example.com"/> <br>
-    <input id='password-loging' placeholder='Password'/> <br>
-    <input id='iniciar-sesion' type="submit"/> <br>
-    <input id='iniciar-sesion' type="submit"/>`
+    <div clas = 'contentLoging'>
+        <input id='name' placeholder='Nombre' /> <br>
+        <input id='email-loging' placeholder="ejemplo@example.com"/> <br>
+        <input id='password-loging' placeholder='Password'/> <br>
+        <input id='iniciar-sesion' type="submit"/> <br>
+        <input id='iniciar-sesion' type="submit"/>
+    </div>`
     const divElement = document.createElement ('div')
     divElement.innerHTML = createAccount;
     return divElement
--- src/componentes/register.js
+++ src/componentes/register.js
@@ -3,12 +3,14 @@ export default () => {
     /* crear cuenta */
     const createAccount = `
     <img src='./img/Placematch__2_-removebg-preview 2.png' alt="logo"> <br>
-    <input id='name' placeholder='Nombre' /> <br>
-    <input id='email' placeholder="Email"/> <br>
-    <input id='phone' placeholder='Phone'/> <br>
-    <input id='password' placeholder='Password'/> <br>
-    <input id='confirmPassword' placeholder='Confirm Password'/> <br>
-    <input id='confirmar' type="submit" />`
+    <div clas = 'contentRegister'>    
+        <input id='name' placeholder='Nombre' /> <br>
+        <input id='email' placeholder="Email"/> <br>
+        <input id='phone' placeholder='Phone'/> <br>
+        <input id='password' placeholder='Password'/> <br>
+        <input id='confirmPassword' placeholder='Confirm Password'/> <br>
+        <input id='confirmar' type="submit" class='registrar'/>
+    </div>`
     const divElement = document.createElement ('div')
     divElement.innerHTML = createAccount;
     return divElement
