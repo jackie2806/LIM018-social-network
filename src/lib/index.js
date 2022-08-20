fdce886d65e1f63cbebce41e38da37258c70891d
jackie2806 <jaishackeline@gmail.com>
2022-08-20 10:39:02 -0500

    mejorando algunas partes de la conexión con Firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 // aqui exportaras las funciones que necesites
-// eslint-disable-next-line import/newline-after-import
+// eslint-disable-next-line import/newline-after-import, import/no-unresolved
 import { auth, state } from '../Firebase/config.js';
 export const authState = state;
