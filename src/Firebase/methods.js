1c8a1fe8edcd519c04aa8095a49a9e23a3e13f6b
jackie2806 <jaishackeline@gmail.com>
2022-09-13 14:19:25 -0500

    aceptando cambios raros

--- src/Firebase/methods.js
+++ src/Firebase/methods.js
@@ -1,10 +1,11 @@
+import { app } from './config.js';
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
-// eslint-disable-next-line import/no-unresolved
+  updateProfile,
 } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-auth.js';
 
 import {
@@ -21,16 +22,37 @@ import {
 // eslint-disable-next-line import/no-unresolved
 } from 'https://www.gstatic.com/firebasejs/9.9.0/firebase-firestore.js';
 
-import { app } from './config.js';
+import { dataRegister } from '../components/Register.js';
+import { onNavigate } from '../main.js';
 
-const saveUserInLocalStorage = (user) => localStorage.setItem('currentUser', JSON.stringify(user));
-const getUserFromLocalStorage = () => JSON.parse(localStorage.getItem('currentUser'));
+export const saveUserInLocalStorage = (user) => localStorage.setItem('currentUser', JSON.stringify(user));
+export const getUserFromLocalStorage = () => JSON.parse(localStorage.getItem('currentUser'));
 
 export const dataBase = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
 // REGISTER
+export const registrar = async () => {
+  const data = dataRegister()
+
+  try {
+    const dataRegister = await createUserWithEmailAndPassword (auth, data.email, data.password)
+
+    await updateProfile(dataRegister.user, {
+      displayName: data.name,
+    })
+    console.log(dataRegister)
+
+    alert(`hola ${data.name} ya estás registrado`)
+    onNavigate('/login')
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert (errorMessage)
+  };
+
+}
 
 // LOGIN
 export const isLoggedIn = () => {
@@ -47,7 +69,7 @@ export const isLoggedIn = () => {
   return false;
 };
 
-const getUser = () => {
+export const getUser = () => {
   if (auth.currentUser) {
     console.log('Firebase', auth.currentUser);
     return auth.currentUser;
@@ -81,13 +103,11 @@ const getUser = () => {
 export const createPost = (post) => addDoc(collection(dataBase, 'post'), { post });
 export const getPosts = () => getDocs(collection(dataBase, 'post'));
 export const onGetPosts = (callback) => onSnapshot(collection(dataBase, 'post'), callback);
+export const deletePost = (id) => {};
 
 export {
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signInWithPopup,
   GoogleAuthProvider,
-  saveUserInLocalStorage,
-  getUserFromLocalStorage,
-  getUser,
+  signInWithPopup
 };
+
