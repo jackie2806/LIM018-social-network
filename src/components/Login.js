643ee35ff7855f531b5e0820a53fe3f9dbcc2a25
Yurema <yuremix.b.s@gmail.com>
2022-09-13 10:17:45 -0500

    ya te puedes registrar y que al iniciar te aparezca con la cuenta registrada

--- src/components/Login.js
+++ src/components/Login.js
@@ -88,15 +88,10 @@ export const Login = () => {
   inputPassword.autocomplete = 'off';
   inputPassword.required = true;
   inputPassword.type = 'password';
-  const imgContrasena = document.createElement('img');
-  imgContrasena.className = 'imgContrasena';
-  imgContrasena.src = '/img/Vector-ojito.png';
-  imgContrasena.alt = 'logoContrasena';
   divInputs.appendChild(divTextEmail);
   divInputs.appendChild(inputEmail);
   divInputs.appendChild(divTextPassword);
   divInputs.appendChild(inputPassword);
-  divInputs.appendChild(imgContrasena);
   const divTextForgotPassword = document.createElement('div');
   divTextForgotPassword.className = 'divForgotPassword';
   divTextForgotPassword.textContent = '¿Olvidaste tu contraseña?';
@@ -127,9 +122,3 @@ export const Login = () => {
 
   return divLogin;
 };
-
-function mostrarContrasena (input, ver) {
-  ver.addEventListener('click', (evt) => {
-    input.type = (input.type === 'password' ? 'text' : 'password');
-  });
-}
